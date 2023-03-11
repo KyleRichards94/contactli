bc10a87fda74077c29cccca98755e2286dad7db6
KyleRichards94 <kyle.richards94@gmail.com>
2023-03-11 23:59:48 +1100

    4 commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import React, { useState, useEffect } from 'react';
-import SearchBar from './components/Search';
 
 const ContactList = () => {                     
   const [contacts, setContacts] = useState([]);                      //contacts[], set through set contacts called through useEffect
@@ -30,41 +29,41 @@ const ContactList = () => {
   return (
     <div id = "main">
 
-      <ul>
-        {filteredContacts.map((contact) => (
-          <li key={contact.id} onClick = { () => contactSelection(contact) } >  
-            <h1 id = 'name' key = {contact.id}> {contact.name}</h1>
-            <p id = 'phone' key = {contact.id}> Phone Number: {contact.phone}</p>
-            <p id = 'email'key = {contact.id}> Email: {contact.email}</p>
-          </li>
-        ))}
-      </ul>
-      
-      <div >
-        <ul id = 'infoBar'>
-
-        <SearchBar  id = 'search' handleSearch = {handleSearch} />
-
-        <h2 id = 'info'>{selectedContact.name}</h2>
-
-        {selectedContact.company ? (
-          <p id= 'info'> {selectedContact.company.catchPhrase} </p>
-        ) : ( null )}
-        
-        {selectedContact.address ? (
-          <p id = 'info'>City:  {selectedContact.address.city} </p>
-        ) : ( <p id = 'info'> No Selection </p>)}
-
-        {selectedContact.address ? (
-          <p id = 'info'>Street:  {selectedContact.address.street} </p>
-        ) : ( null )}
-        
-        {selectedContact.company ? (
-          <p id = 'info'>Company:  {selectedContact.company.name} </p>
-        ) : ( null )}
-        
+        <ul>
+          {filteredContacts.map((contact) => (
+            <li key={contact.id} onClick = { () => contactSelection(contact) } >  
+              <h1 id = 'name' key = {contact.id}> {contact.name}</h1>
+              <p id = 'phone' key = {contact.id}> Phone Number: {contact.phone}</p>
+              <p id = 'email'key = {contact.id}> Email: {contact.email}</p>
+            </li>
+          ))}
         </ul>
-      </div>
+        
+        <div >
+          <ul id = 'infoBar'>
+
+          <input type="text" placeholder="Search: Name, Email or phone" onChange={(event) => handleSearch(event.target.value)}/>
+
+          <h2 id = 'info'>{selectedContact.name}</h2>
+
+          {selectedContact.company ? (
+            <p id= 'info'> {selectedContact.company.catchPhrase} </p>
+          ) : ( null )}
+          
+          {selectedContact.address ? (
+            <p id = 'info'>City:  {selectedContact.address.city} </p>
+          ) : ( <p id = 'info'> No Selection </p>)}
+
+          {selectedContact.address ? (
+            <p id = 'info'>Street:  {selectedContact.address.street} </p>
+          ) : ( null )}
+          
+          {selectedContact.company ? (
+            <p id = 'info'>Company:  {selectedContact.company.name} </p>
+          ) : ( null )}
+          
+          </ul>
+        </div>
     </div>
   );
 };
